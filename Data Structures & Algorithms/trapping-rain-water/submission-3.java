51c83622280b8e8cc4d2027eef2ba9124cc65e29
Icarus <samalexson1214@gmail.com>
2026-06-22 09:40:52 +0800

    Add: trapping-rain-water - submission-3

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-3.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int trap(int[] height) {
+        // read through the array from left to right (each number : cols)
+        // Two pointers approach
+        int left = 1;
+        int right = height.length - 2;
+
+        int prefix = height[left - 1];
+        int suffix = height[right + 1];
+
+        int rain = 0;
+        while (left <= right) {
+            if (suffix <= prefix) {
+                rain += Math.max(0, suffix - height[right]); // Add water
+                suffix = Math.max(suffix, height[right]); // update right-suffix:max
+                right--; //Post-operations for better runtime, less memory usage for pointing variables stored
+            }
+            else {
+                rain += Math.max(0, prefix - height[left]);
+                prefix = Math.max(prefix, height[left]);
+                left++;
+            }
+        }
+        return rain;
+    }
+}
