36cefa1f8c71a38059cccb40f61d980301b8bf70
Icarus <samalexson1214@gmail.com>
2026-06-22 10:02:21 +0800

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        // iterate forward day (ith) - no backwards, find best buy first then best sell 
+        // Minimize buy, maximize sell
+        int min = prices[0];
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) { // Start from next element
+            if (prices[i] < min) min = prices[i];
+            profit = Math.max(profit, prices[i] - min);
+        }
+        return profit;
+    }
+}
